408baa1009130029285e080a4b738fa582ba73cb
jsyum <jennifersyum@gmail.com>
2019-04-21 23:39:05 -0400

    db and server done, writing orm

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+var connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+var orm = {
+  selectAll: function(table, cb) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [table], function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      cb(res);
+    });
+  },
+  insertOne: function(table, column, value, cb) {
+    var queryString = "INSERT INTO ?? VALUES ?";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log(result);
+      cb(res);
+    });
+  }
+  //updateOne:
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+
+SELECT * FROM burgers;
