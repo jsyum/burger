e1f49fafd88a422429d59f42508cf1c5c45124c5
jsyum <jennifersyum@gmail.com>
2019-04-23 18:36:51 -0400

    writing orm

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,45 @@
 //importing the connection
 var connection = require("./connection.js");
 
-// Object Relational Mapper (ORM)
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+  var arr = [];
 
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+// Object Relational Mapper (ORM)
 // The ?? signs are for swapping out table or column names
 // The ? signs are for swapping out other values
 // These help avoid SQL injection
@@ -14,18 +51,17 @@ var orm = {
   //     cb(null, data);
   //   });
   // }
-  selectAll: function(table, cb) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [table], function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      cb(res);
-    });
-  }
+  // selectAll: function(table, cb) {
+  //   var queryString = "SELECT * FROM ??";
+  //   connection.query(queryString, table, function(err, res) {
+  //     if (err) throw err;
+  //     console.log(res);
+  //     cb(res);
+  //   });
+  // },
   // insertOne: function(table, column, value, cb) {
   //   var queryString = "INSERT INTO ?? VALUES ?";
-  //   console.log(queryString);
-  //   connection.query(queryString, [whatToSelect, table, orderCol], function(
+  //   connection.query(queryString, [table, column, value], function(
   //     err,
   //     result
   //   ) {
@@ -33,8 +69,66 @@ var orm = {
   //     console.log(result);
   //     cb(res);
   //   });
-  // }
-  //updateOne:
+  // },
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  deleteOne: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
 };
 
 //exporting the orm
