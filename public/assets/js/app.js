c08de4b2eb7eef1576ffc6ea37f0f8ac581c4bdb
jsyum <jennifersyum@gmail.com>
2019-04-25 17:27:14 -0400

    working on eat button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,4 +19,24 @@ $(function() {
       location.reload();
     });
   });
+  $("#eat").on("click", function(event) {
+    {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+
+      var newDevouredState = {
+        devoured: newDevoured
+      };
+
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(function() {
+        console.log("changed devoured state to", newDevouredState);
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    }
+  });
 });
