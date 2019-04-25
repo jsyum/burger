60c50c7b427c95678e1100e9197c526030907f75
jsyum <jennifersyum@gmail.com>
2019-04-25 18:58:23 -0400

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -13,10 +13,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
+    host: "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ba6p7ixsacz6yfhg",
+    password: "vuvt6okzfv38a9x5",
+    port: 3306,
+    database: "rp2fz8y8duroutg3"
   });
 }
 
